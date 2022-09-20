81cc02e18f4cd12d31bc1a1c3f4f8daceb4d4e1d
0rdinary <ac980925@gmail.com>
2022-09-20 18:31:15 +0900

    initialize frontend

--- src/main/java/com/chickenbandits/daeily/DaeilyApplication.java
+++ src/main/java/com/chickenbandits/daeily/DaeilyApplication.java
@@ -2,16 +2,9 @@ package com.chickenbandits.daeily;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class DaeilyApplication {
-
-    @GetMapping("/")
-    public String hello() {
-        return "Test Hello";
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(DaeilyApplication.class, args);
     }
---
+++ src/main/java/com/chickenbandits/daeily/web/IndexController.java
@@ -0,0 +1,10 @@
+package com.chickenbandits.daeily.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {
+
+}

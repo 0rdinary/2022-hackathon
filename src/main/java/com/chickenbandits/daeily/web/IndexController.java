87b6066d024f5071a5e1deab360e4b7d6f28d5a0
0rdinary <ac980925@gmail.com>
2022-09-20 19:18:13 +0900

    Connect front and backend

--- src/main/java/com/chickenbandits/daeily/web/IndexController.java
+++ src/main/java/com/chickenbandits/daeily/web/IndexController.java
@@ -1,10 +1,15 @@
 package com.chickenbandits.daeily.web;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class IndexController {
 
+    @GetMapping("/api")
+    public String index() {
+        return "index";
+    }
 }
